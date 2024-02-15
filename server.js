b69ddc63d5f9179d356169b35247b9a438903538
Jesus M <marquez.jay444@gmail.com>
2024-02-14 22:15:59 -0600

    logic to parse json in GET routes

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+const fs = require('fs')
 const express = require('express');
 const app = express();
 const path = require('path');
@@ -10,7 +12,49 @@ app.use(express.json())
 
 // //  API Routes
 app.get('/api/animals', (req, res) => {
-   res.json(animalData)
+ // Read animals.json contents
+  fs.readFile(path.join(__dirname, 'animals.json'), 'utf-8', function(err, data){
+    if (err) {
+       res.status(500).json(err)
+       return
+    }
+    const json = JSON.parse(data)
+    // res.json the parsed array
+    res.json(json)
+  })
+})
+
+
+app.post('/api/animals', (req,res) =>{
+  const { name, age, type } = req.body
+  
+  if (!name || !type || !age) {
+    res.status(400).json({ERROR : 'Missing name, type, or age'})
+    return
+  }
+  
+  // Read the last object from the json file and get its id
+  const lastAnimal = animalData[animalData.length - 1];
+  const lastId = lastAnimal.id;
+  
+  // Assign the new id as the last id plus one
+  const newAnimal = {
+    id: lastId + 1,
+    ...req.body,
+  }
+  //  read contents of animals.JSON file
+  // parse string into JSON
+  // push newAnimal into JSON
+  // stringify animal array 
+  // save file 
+  
+  
+  // animalData.push(newAnimal)
+  
+  // res.json(newAnimal)
+  // res.send(req.body) also works here but you will only see the json in when the client sends a GET req
+  
+  
 })
 
 app.get('/api/animals/:animalType', (req, res) => {
@@ -20,39 +64,24 @@ app.get('/api/animals/:animalType', (req, res) => {
     res.status(400).send(`<h1 style="display:flex; justify-content:center; padding-top:183px; font-size:69px "><span style="color:red">ERROR</span>: "${animalType}" is not a valid input. Make sure to use lower-case letters only!</h1>`)
     return
   }
-
-  const results = animalData.filter(a => a.type === animalType)
-
-  if(results.length === 0) {
-    return res.status(404).send(`<h1 style="display:flex; justify-content:center; padding-top:183px; font-size:69px "><span style="color:red">ERROR</span>: "${animalType}" not found</h1>`)
-  }
-
-  res.json(results)
-})
-
-app.post('/api/animals', (req,res) =>{
-  const { name, age, type } = req.body
-
-  if (!name || !type || !age) {
-    res.status(400).json({ERROR : 'Missing name, type, or age'})
-    return
-  }
-
-   // Read the last object from the json file and get its id
-   const lastAnimal = animalData[animalData.length - 1];
-   const lastId = lastAnimal.id;
- 
-   // Assign the new id as the last id plus one
-   const newAnimal = {
-     id: lastId + 1,
-     ...req.body,
-   }
-
-  animalData.push(newAnimal)
-
-  res.json(newAnimal)
-  // res.send(req.body) also works here but you will only see the json in when the client sends a GET req
-
+  
+      // Read animals.json contents
+      fs.readFile(path.join(__dirname, 'animals.json'), 'utf-8' ,function(err, data) {
+        if (err) {
+          res.status(500).json(err)
+          return
+        }
+        const animalArray = JSON.parse(data)
+        // res.json the parsed array
+        const results = animalArray.filter(a => a.type === animalType)
+      
+        if(results.length === 0) {
+          return res.status(404).send(`<h1 style="display:flex; justify-content:center; padding-top:183px; font-size:69px "><span style="color:red">ERROR</span>: "${animalType}" not found</h1>`)
+        }
+      
+        res.json(results)
+      }) 
+      
 
 })
 
